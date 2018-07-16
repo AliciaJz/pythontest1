2b9385b770acfa613f124b506335ffbd44fc6ca6
AliciaJz <zilaphono@gmail.com>
2018-07-16 17:37:27 -0500

    funciones usar def

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-a = 0
-for i in range(5):
-  a+=1
-  print(a)
+# a = 0
+# for i in range(5):
+#   a+=1
+#   print(a)
+
+
+def mensaje():
+  print("mensaje1")
+  print("mensaje2")
+  print("mensaje3")
+
+mensaje()
