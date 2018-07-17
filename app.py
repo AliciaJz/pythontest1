35f933f9182daf15269df113d4dc89848076a6b7
AliciaJz <zilaphono@gmail.com>
2018-07-16 20:25:58 -0500

    tuplas

--- app.py
+++ app.py
@@ -26,32 +26,57 @@
 
 # print(summation(5, 7.3))
 
-my_list=["Lore", "Betty", "Alicia", "Virgin"]
+# my_list=["Lore", "Betty", "Alicia", "Virgin"]
 
-my_list.append("Din")
-my_list.insert(2, "Din")
-my_list.extend(["Norma", "Silvia", "Raul"])
+# my_list.append("Din")
+# my_list.insert(2, "Din")
+# my_list.extend(["Norma", "Silvia", "Raul"])
 
-print(my_list[:])
-my_list.pop()
+# print(my_list[:])
+# my_list.pop()
 
-print(my_list[:])
-print(my_list[0])
-print(my_list[-1])
-print(my_list[0:2])
-print(my_list[:2])
-print(my_list[2:])
-print(my_list.index("Alicia"))
-print("Norma" in my_list)
-print("Laura" in my_list)
+# print(my_list[:])
+# print(my_list[0])
+# print(my_list[-1])
+# print(my_list[0:2])
+# print(my_list[:2])
+# print(my_list[2:])
+# print(my_list.index("Alicia"))
+# print("Norma" in my_list)
+# print("Laura" in my_list)
 
-my_list.remove("Din")
-print(my_list[:])
+# my_list.remove("Din")
+# print(my_list[:])
 
-my_list_1=[5, True, 7.9]
+# my_list_1=[5, True, 7.9]
 
-my_list_2=[5, True, 7.9] * 3
-print(my_list_2[:])
+# my_list_2=[5, True, 7.9] * 3
+# print(my_list_2[:])
 
-all_lists=my_list+my_list_1
-print(all_lists[:])
+# all_lists=my_list+my_list_1
+# print(all_lists[:])
+
+my_tuple=("Alicia", 13,7,79, "Alicia", "Alicia")
+print(my_tuple)
+my_list_tuple=list(my_tuple)
+print(my_list_tuple)
+
+my_list_tuple1=["Alicia", 13,7,79]
+print(my_list_tuple1)
+my_tuple1=tuple(my_list_tuple1)
+print(my_tuple1)
+
+print("Alicia" in my_tuple)
+
+print(my_tuple.count(13))
+print(my_tuple.count("Alicia"))
+
+print(len(my_tuple))
+
+my_tuple=("Alicia", 13,7,79)
+name, day, month, year= my_tuple
+
+print(name)
+print(year)
+print(month)
+print(day)
