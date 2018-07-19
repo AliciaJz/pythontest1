d477ba6942aefd8ea168c91125ba3e4f56ca1b5a
AliciaJz <zilaphono@gmail.com>
2018-07-19 18:55:34 -0500

    encapsulamiento

--- poo.py
+++ poo.py
@@ -2,26 +2,26 @@ class Car():
 
   def __init__(self):
 
-    self.lenChassis=250
-    self.widChassis=120
-    self.wheels=4
-    self.running=False
+    self.__lenChassis=250
+    self.__widChassis=120
+    self.__wheels=4
+    self.__running=False
 
   def start(self, run):
-    self.running=run
+    self.__running=run
 
-    if(self.running):
+    if(self.__running):
       return "Car running"
     else:
       return "Car stopped"
 
   def state(self):
-    print("Car has", my_car.wheels, "wheels, a width of ", self.widChassis, "and length of", self.lenChassis)
+    print("Car has", self.__wheels, "wheels, a width of", self.__widChassis, "and length of", self.__lenChassis)
 
 my_car=Car()
 
-print("Car length: ", my_car.lenChassis)
-print("Has", my_car.wheels, "wheels")
+# print("Car length: ", my_car.__lenChassis)
+# print("Has", my_car.__wheels, "wheels")
 print(my_car.start(True))
 my_car.state()
 
@@ -29,8 +29,9 @@ my_car.state()
 print("--------Second car--------")
 
 my_car2=Car()
-print("Car length: ", my_car2.lenChassis)
-print("Has", my_car2.wheels, "wheels")
+# print("Car length: ", my_car2.lenChassis)
+# print("Has", my_car2.wheels, "wheels")
 print(my_car2.start(False))
+my_car2.wheels=5
 my_car2.state()
 
