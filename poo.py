fd948d84959bae92780eca0f284de0926bd76afa
AliciaJz <zilaphono@gmail.com>
2018-07-20 19:02:33 -0500

    herencia 3 usando super e isinstance

--- poo.py
+++ poo.py
@@ -52,64 +52,97 @@
 
 # my_car2.state()
 
-class Vehicles():
-  def __init__(self, brand, model):
+# class Vehicles():
+#   def __init__(self, brand, model):
 
-    self.brand=brand
-    self.model=model
-    self.running=False
-    self.accelerating=False
-    self.stoping=False
+#     self.brand=brand
+#     self.model=model
+#     self.running=False
+#     self.accelerating=False
+#     self.stoping=False
 
-  def start_car(self):
-    self.running=True
+#   def start_car(self):
+#     self.running=True
 
-  def accelerate(self):
-    self.accelerating=True
+#   def accelerate(self):
+#     self.accelerating=True
 
-  def stop_car(self):
-    self.stoping=True
+#   def stop_car(self):
+#     self.stoping=True
   
-  def state(self):
-    print("Brand: ", self.brand, "\nModel: ", self.model, "\nRunning: ", self.running, "\nAccelerating: ", self.accelerating, "\nStoping: ", self.stoping,)
+#   def state(self):
+#     print("Brand: ", self.brand, "\nModel: ", self.model, "\nRunning: ", self.running, "\nAccelerating: ", self.accelerating, "\nStoping: ", self.stoping,)
 
-class Minivan(Vehicles):
-  def carry(self, carry):
-    self.carried=carry
-    if(self.carried):
-      return "Minivan is carring something"
-    else:
-      return "Minivan is carring nothing"
+# class Minivan(Vehicles):
+#   def carry(self, carry):
+#     self.carried=carry
+#     if(self.carried):
+#       return "Minivan is carring something"
+#     else:
+#       return "Minivan is carring nothing"
 
-class Motorcycle(Vehicles):
-  front_wheel=""
-  def horsey(self):
-    self.front_wheel="Running using only one wheel"
+# class Motorcycle(Vehicles):
+#   front_wheel=""
+#   def horsey(self):
+#     self.front_wheel="Running using only one wheel"
 
-  def state(self):
-    print("Brand: ", self.brand, "\nModel: ", self.model, "\nRunning: ", self.running, "\nAccelerating: ", self.accelerating, "\nStoping: ", self.stoping, "\nSpecial method: ", self.front_wheel)
+#   def state(self):
+#     print("Brand: ", self.brand, "\nModel: ", self.model, "\nRunning: ", self.running, "\nAccelerating: ", self.accelerating, "\nStoping: ", self.stoping, "\nSpecial method: ", self.front_wheel)
 
-class Electric_vehicles():
-  def __init__(self):
-    self.autonomy=100
+# class Electric_vehicles():
+#   def __init__(self):
+#     self.autonomy=100
 
-  def charge_battery(self):
-    self.charging=True
+#   def charge_battery(self):
+#     self.charging=True
 
-my_motorcycle1=Motorcycle("Lou's", "bike")
+# my_motorcycle1=Motorcycle("Lou's", "bike")
 
-my_motorcycle1.horsey()
+# my_motorcycle1.horsey()
 
-my_motorcycle1.state()
+# my_motorcycle1.state()
 
 
-my_minivan1=Minivan("Deb's", "van")
+# my_minivan1=Minivan("Deb's", "van")
 
-my_minivan1.start_car()
-my_minivan1.state()
-print(my_minivan1.carry(True))
+# my_minivan1.start_car()
+# my_minivan1.state()
+# print(my_minivan1.carry(True))
 
-class Electric_bike(Electric_vehicles, Vehicles):
-  pass
+# class Electric_bike(Electric_vehicles, Vehicles):
+#   pass
 
-my_electricbike1=Electric_bike()
+# my_electricbike1=Electric_bike()
+
+class Person():
+  def __init__(self, name, age, city):
+    
+    self.name=name
+    self.age=age
+    self.city=city
+
+  def description(self):
+    print("Name:", self.name, " Age:", self.age, "Residency:", self.city)
+
+class Employee(Person):
+  def __init__(self, salary, years, employee_name, employee_age, employee_years):
+
+    super().__init__(employee_name, employee_age, employee_years)
+
+    self.salary=salary
+    self.years=years
+
+  def description(self):
+    super().description()
+    print("Salary:", self.salary, " Years working:", self.years)
+
+Carol=Employee(1500, 1, "Carol", 45, "NY")
+Carol2=Person("Carol", 45, "NY")
+
+Carol.description()
+Carol2.description()
+
+print(isinstance(Carol, Employee))
+print(isinstance(Carol, Person))
+print(isinstance(Carol2, Employee))
+print(isinstance(Carol2, Person))
